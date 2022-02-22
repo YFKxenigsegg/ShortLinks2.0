61fe9ac4c4b9949c6a7b335d1fbce87755fd9817
Korotygin Matvey <matveykaklanter@mail.ru>
2022-02-23 02:08:40 +0300

    add migrations and 'RollBackController'

---
+++ ShortLinks2.0.Persistence.Migrations/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShortLinks.Persistence.Migrations;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMigrations(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddFluentMigratorCore()
+            .ConfigureRunner(config =>
+                config.AddSqlServer()
+                .WithGlobalConnectionString(configuration.GetConnectionString("DefaultConnection"))
+                .ScanIn()
+
+            );
+
+
+        return services;
+    }
+}
---
+++ ShortLinks2.0.Persistence.Migrations/RollBackController.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ShortLinks.Persistence.Migrations;
+[ApiController]
+[Route("api/[controller]")]
+public class RollBackController : ControllerBase
+{
+    private readonly ILogger<RollBackController> _logger;
+    private readonly IMigrationRunner _migrationRunner;
+
+    public RollBackController(
+        ILogger<RollBackController> logger
+        , IMigrationRunner migrationRunner
+        ) => (_logger, _migrationRunner) = (logger, migrationRunner);
+
+    //TODO
+}
